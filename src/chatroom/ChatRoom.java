1cf8a8caf9f528aaef3e266053caf319ffb3e178
deepseapixie <kia.miller818@gmail.com>
2018-01-31 22:56:21 -0500

    Add availableRep SinglyLinkedList

--- src/chatroom/ChatRoom.java
+++ src/chatroom/ChatRoom.java
@@ -1,6 +1,7 @@
 package chatroom;
 
 public class ChatRoom {
+	static SinglyLinkedList<Representative> availableReps = new SinglyLinkedList<Representative>();
 	
 	public static void main(String[] args) {
 		
